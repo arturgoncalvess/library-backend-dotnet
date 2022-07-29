9f02fccbd3976d467e4843fbe458816f3bc37201
ArturGoncalvesDev <arturgoncalves00000@gmail.com>
2022-07-29 10:57:04 -0300

    Adding my library on github

---
+++ Livraria.API/Controllers/UsuarioController.cs
@@ -0,0 +1,105 @@
+﻿using Livraria.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Livraria.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+
+        public List<Usuario> Usuarios = new List<Usuario>()
+        {
+            new Usuario()
+            {
+                Id = 1,
+                Nome = "Artur",
+                Cidade = "Fortaleza",
+                Endereco = "Rua A",
+                Email = "Artur@gmail.com"
+            },
+            new Usuario()
+            {
+                Id = 2,
+                Nome = "Art",
+                Cidade = "Fortal",
+                Endereco = "Rua B",
+                Email = "Art@gmail.com"
+            },
+            new Usuario()
+            {
+                Id = 3,
+                Nome = "Ark",
+                Cidade = "Fort",
+                Endereco = "Rua C",
+                Email = "Ark@gmail.com"
+            },
+            new Usuario()
+            {
+                Id = 5,
+                Nome = "Ana",
+                Cidade = "Fort",
+                Endereco = "Rua D",
+                Email = "Anak@gmail.com"
+            }
+        };
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            return Ok(Usuarios);
+        }
+
+        // api/usuario/byId
+        [HttpGet("byId")]
+        public IActionResult GetById(int id)
+        {
+            var usuario = Usuarios.FirstOrDefault(u => u.Id == id);
+            if (usuario == null) return BadRequest("O usuário não foi encontrado :(");
+            return Ok(usuario);
+        }
+
+        // api/usuario/byName
+        [HttpGet("byName")]
+        public IActionResult GetByName(string nome)
+        {
+            var usuario = Usuarios.Where(u => u.Nome.Contains(nome));
+            if (usuario == null) return BadRequest("Nenhum aluno com " + usuario + "encontrado");
+            return Ok(usuario);
+        }
+
+        // POST
+        [HttpPost]
+        public IActionResult Post(Usuario usuario)
+        {
+            return Ok(usuario);
+        }
+
+        // POST
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, Usuario usuario)
+        {
+            return Ok(usuario);
+        }
+
+        // PUT
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Usuario usuario)
+        {
+            return Ok(usuario);
+        }
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ Livraria.API/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿namespace Livraria.API.Models
+{
+    public class Usuario
+    {
+        public Usuario() { }
+        public Usuario(int id, string nome, string cidade, string endereco, string email)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Cidade = cidade;
+            this.Endereco = endereco;
+            this.Email = email;
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cidade { get; set; }
+        public string Endereco { get; set; }    
+        public string Email { get; set; }
+    }
+}
