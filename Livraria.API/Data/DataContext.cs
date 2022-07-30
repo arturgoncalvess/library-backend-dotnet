0fe8fa669094c3ade7faeb4ec282739012d37f35
ArturGoncalvesDev <arturgoncalves00000@gmail.com>
2022-07-30 12:28:35 -0300

    Create data context, migrations and SQLite database

---
+++ Livraria.API/Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Livraria.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Livraria.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //    builder.Entity<UsuarioAluguel>()
+            //        .HasKey(AD => new { AD.UsuarioId, AD.AluguelId });
+
+
+            builder.Entity<Usuario>()
+                .HasData(new List<Usuario>(){
+                    new Usuario(1, "Artur", "Cascavel", "Rua A", "Artur@gmail.com"),
+                    new Usuario(2, "Ana", "Caucaia", "Rua T", "Ana@gmail.com"),
+                    new Usuario(3, "Vilma", "São Paulo", "Rua K", "Vilma@gmail.com"),
+                    new Usuario(4, "Vitor", "Fortaleza", "Rua E", "Vitor@gmail.com"),
+                });
+        }
+    }
+}
+
---
+++ Livraria.API/Migrations/20220729184403_init.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Livraria.API.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(nullable: true),
+                    Cidade = table.Column<string>(nullable: true),
+                    Endereco = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Usuarios",
+                columns: new[] { "Id", "Cidade", "Email", "Endereco", "Nome" },
+                values: new object[] { 1, "Cascavel", "Artur@gmail.com", "Rua A", "Artur" });
+
+            migrationBuilder.InsertData(
+                table: "Usuarios",
+                columns: new[] { "Id", "Cidade", "Email", "Endereco", "Nome" },
+                values: new object[] { 2, "Caucaia", "Ana@gmail.com", "Rua T", "Ana" });
+
+            migrationBuilder.InsertData(
+                table: "Usuarios",
+                columns: new[] { "Id", "Cidade", "Email", "Endereco", "Nome" },
+                values: new object[] { 3, "São Paulo", "Vilma@gmail.com", "Rua K", "Vilma" });
+
+            migrationBuilder.InsertData(
+                table: "Usuarios",
+                columns: new[] { "Id", "Cidade", "Email", "Endereco", "Nome" },
+                values: new object[] { 4, "Fortaleza", "Vitor@gmail.com", "Rua E", "Vitor" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
