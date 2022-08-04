fcca490fdbc482dad3a140f89617440aa9eec4d7
ArturGoncalvesDev <arturgoncalves00000@gmail.com>
2022-08-03 23:40:34 -0300

    Update controller

---
+++ Livraria.API/Controllers/EditoraController.cs
@@ -0,0 +1,85 @@
+﻿using Livraria.API.Data;
+using Livraria.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Livraria.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EditoraController : ControllerBase
+    {
+        private readonly IRepository _repo;
+
+        public EditoraController(IRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            var result = _repo.GetAllEditoras();
+            return Ok(result);
+        }
+
+        // api/editora/byId
+        [HttpGet("byId")]
+        public IActionResult GetById(int id)
+        {
+            var editora = _repo.GetEditoraById(id);
+            if (editora == null) return BadRequest("A editora não foi encontrado :( 1");
+            return Ok(editora);
+        }
+
+        // POST
+        [HttpPost]
+        public IActionResult Post(Editora editora)
+        {
+            _repo.Add(editora);
+            if (_repo.SaveChanges())
+            {
+                return Ok(editora);
+            }
+
+            return BadRequest("Editora não foi cadastrado");
+        }
+
+        // PUT
+        [HttpPut("{id}")]
+        public IActionResult Put(int id)
+        {
+            var edi = _repo.GetEditoraById(id);
+            if (edi == null) return BadRequest("O editora não foi encontrado :( 4");
+
+            _repo.Update(edi);
+            if (_repo.SaveChanges())
+            {
+                return Ok("Editora foi atualizado: " + edi);
+            }
+
+            return BadRequest("Editora não foi atualizado");
+        }
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var edi = _repo.GetEditoraById(id);
+            if (edi == null) return BadRequest("O editora não foi encontrado :( 5");
+
+            _repo.Delete(edi);
+            if (_repo.SaveChanges())
+            {
+                return Ok("A editora foi deletado");
+            }
+
+            return BadRequest("A editora não foi deletado");
+        }
+
+    }
+}
+
--- Livraria.API/Controllers/UsuarioController.cs
+++ Livraria.API/Controllers/UsuarioController.cs
@@ -1,7 +1,7 @@
 ﻿using Livraria.API.Data;
 using Livraria.API.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,79 +12,73 @@ namespace Livraria.API.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
-        private readonly DataContext _context;
+        private readonly IRepository _repo;
 
-        public UsuarioController(DataContext context)
+        public UsuarioController(IRepository repo)
         {
-            _context = context;
+            _repo = repo;
         }
-
 
         [HttpGet]
 
         public IActionResult Get()
         {
-            return Ok(_context.Usuarios);
+            var result = _repo.GetAllUsuarios();
+            return Ok(result);
         }
 
-        // api/usuario/byId
-        [HttpGet("byId")]
+        // api/usuario
+        [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            var usuario = _context.Usuarios.FirstOrDefault(u => u.Id == id);
-            if (usuario == null) return BadRequest("O usuário não foi encontrado :( 1");
-            return Ok(usuario);
+            var user = _repo.GetUsuarioById(id);
+            if (user == null) return BadRequest("O usuário não foi encontrado :( 1");
+            return Ok(user);
         }
 
-        // api/usuario/byName
-        [HttpGet("byName")]
-        public IActionResult GetByName(string nome)
-        {
-            var usuario = _context.Usuarios.Where(u => u.Nome.Contains(nome));
-            if (usuario == null) return BadRequest("Nenhum aluno com " + usuario + "encontrado 2");
-            return Ok(usuario);
-        }
-
-        // POST
+        // api/usuario
         [HttpPost]
         public IActionResult Post(Usuario usuario)
         {
-            _context.Add(usuario);
-            _context.SaveChanges();
-            return Ok(usuario);
+            _repo.Add(usuario);
+            if(_repo.SaveChanges())
+            {
+                return Ok(usuario);
+            }
+
+            return BadRequest("O usuário não foi cadastrado");
         }
 
-        // POST
-        [HttpPatch("{id}")]
-        public IActionResult Patch(int id, Usuario usuario)
-        {
-            var usua = _context.Usuarios.FirstOrDefault(u => u.Id == id);
-            if (usuario == null) return BadRequest("O usuário não foi encontrado :( 3");
-            _context.Update(usuario);
-            _context.SaveChanges();
-            return Ok(usuario);
-        }
-
-        // PUT
+        // api/usuario
         [HttpPut("{id}")]
         public IActionResult Put(int id, Usuario usuario)
         {
-            var usua = _context.Usuarios.FirstOrDefault(u => u.Id == id);
-            if (usuario == null) return BadRequest("O usuário não foi encontrado :( 4");
-            _context.Update(usuario);
-            _context.SaveChanges();
-            return Ok(usuario);
+            var user = _repo.GetUsuarioById(id);
+            if (user == null) return BadRequest("O usuário não foi encontrado :( 4");
+
+            _repo.Update(user);
+            if (_repo.SaveChanges())
+            {
+                return Ok("O usuário foi atualizado");
+            }
+
+            return BadRequest("O usuário não foi atualizado");
         }
 
-        // DELETE
+        // api/usuario
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            var usuario = _context.Usuarios.FirstOrDefault(u => u.Id == id);
-            if (usuario == null) return BadRequest("O usuário não foi encontrado :( 5");
-            _context.Remove(usuario);
-            _context.SaveChanges();
-            return Ok();
+            var user = _repo.GetUsuarioById(id);
+            if (user == null) return BadRequest("O usuário não foi encontrado :( 5");
+
+            _repo.Delete(user);
+            if (_repo.SaveChanges())
+            {
+                return Ok("O usuário foi deletado");
+            }
+
+            return BadRequest("O usuário não foi deletado");
         }
 
     }
