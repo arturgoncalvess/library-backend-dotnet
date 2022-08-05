a856e9ca43f3437573005be5419381b30f210f39
ArturGoncalvesDev <arturgoncalves00000@gmail.com>
2022-08-05 11:27:07 -0300

    Create model and rental controller

---
+++ Livraria.API/Controllers/RentalController.cs
@@ -0,0 +1,87 @@
+﻿using Livraria.API.Data;
+using Livraria.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Livraria.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalController : ControllerBase
+    {
+        private readonly IRepository _repo;
+
+        public RentalController(IRepository repo)
+        {
+            _repo = repo;
+        }
+
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            var result = _repo.GetAllRentals();
+            return Ok(result);
+        }
+
+
+        // api/rental/ById
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var ren = _repo.GetRentalById(id, id);
+            if (ren == null) return BadRequest("Rental not found :(");
+            return Ok(ren);
+        }
+
+        // api/rental
+        [HttpPost]
+        public IActionResult Post(Rental rental)
+        {
+            _repo.Add(rental);
+            if (_repo.SaveChanges())
+            {
+                return Ok(rental);
+            }
+
+            return BadRequest("Unable to register rental :(");
+        }
+
+        // api/rental
+        [HttpPut("{id}")]
+        public IActionResult Put(int id)
+        {
+            var ren = _repo.GetRentalById(id, id);
+            if (ren == null) return BadRequest("Rental not found :(");
+            
+            _repo.Update(ren);
+            if (_repo.SaveChanges())
+            {
+                return Ok("Updated rental");
+            }
+
+            return BadRequest("Unable to update rental :(");
+        }
+
+        // api/rental
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var ren = _repo.GetRentalById(id, id);
+            if (ren == null) return BadRequest("Rental not found :(");
+            
+            _repo.Delete(ren);
+            if (_repo.SaveChanges())
+            {
+                return Ok("Rental deleted");
+            }
+
+            return BadRequest("Unable to delete rental :(");
+
+        }
+    }
+}
---
+++ Livraria.API/Models/Rental.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Livraria.API.Models
+{
+    public class Rental
+    {
+        public Rental() { }
+        public Rental(int id, int user_id, int book_id, int rental_date, int forecast_date, int return_date)
+        {
+            this.Id = id;
+            this.UserId = user_id;
+            this.BookId = book_id;
+            this.Rental_Date = rental_date;
+            this.Forecast_Date = forecast_date;
+            this.Return_date = return_date;
+        }
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+        public int Rental_Date { get; set; }
+        public int Forecast_Date { get; set; }
+        public int Return_date { get; set; }
+    }
+}
