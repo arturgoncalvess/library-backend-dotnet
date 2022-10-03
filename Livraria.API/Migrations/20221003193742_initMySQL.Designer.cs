1e0f4aac72714f40bd0b588d6aa9057ebeb8a7cf
ArturGoncalvesDev <arturgoncalves00000@gmail.com>
2022-10-03 17:14:42 -0300

    prototype 1.2

--- Livraria.API/Migrations/20221003193742_initMySQL.Designer.cs
+++ Livraria.API/Migrations/20221003193742_initMySQL.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Livraria.API.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    [Migration("20221003175345_initMySQL")]
+    [Migration("20221003193742_initMySQL")]
     partial class initMySQL
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
